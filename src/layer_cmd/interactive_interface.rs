23486b60c5cefb0e8b173f2a1a47eb547cf1ed64
chorogchip <chorogchip@naver.com>
2023-09-26 12:10:04 +0900

    edited interactive interface

--- src/layer_cmd/interactive_interface.rs
+++ src/layer_cmd/interactive_interface.rs
@@ -1,14 +1,9 @@
 
 use std::io;
 use std::io::Write;
+use std::str::FromStr;
 use crate::layer_option::options;
 use crate::layer_option::errors;
-
-fn read_int() -> i64 {
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf).expect("failed to read line");
-    return buf.trim().parse().unwrap_or(-1);
-}
 
 fn flush_output() {
     if let Err(error) = io::stdout().flush() {
@@ -16,13 +11,35 @@ fn flush_output() {
     }
 }
 
+fn read_val<T: FromStr>(error_val: T) -> T {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("failed to read line");
+    return buf.trim().parse().unwrap_or(error_val);
+}
+
+fn read_string() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("failed to read line");
+    return String::from(buf.trim());
+}
+
+fn get_val<T: FromStr>(message: &str, error_val: T) -> T {
+    print!("{}>> ", message);
+    flush_output();
+    return read_val(error_val);
+}
+
+fn get_strnig(message: &str) -> String {
+    print!("{}>> ", message);
+    flush_output();
+    return read_string();
+}
+
 pub fn execute_interactive(options: &mut options::Options) {
 
     print!("entered interactive mode\n");
 
-    loop {
-
-        print!(
+    loop { match get_val(
 "\n- Interactive Mode Options -
 1) input file option
 2) output file option
@@ -31,114 +48,81 @@ pub fn execute_interactive(options: &mut options::Options) {
 5) performance option
 6) complete and execute!
 7) exit and cancel execution
->> ");
-
-        flush_output();
-        
-        match read_int() {
-            1 => execute_interactive_input_file(options),
-            2 => execute_interactive_output_file(options),
-            3 => execute_interactive_scene_structure(options),
-            4 => execute_interactive_rendering(options),
-            5 => execute_interactive_performance(options),
-            6 => {
-                print!("> Completed interactive mod and Starting to Execute!\n");
-                break;
-            }
-            7 => {
-                options.errors.add_serious_error(errors::SeriousErrors::ExplicitExit(
-                    String::from("exit typed on interactive mod")));
-                break;
-            },
-            _ => print!("Wrong input\n"),
+", -1) {
+        1 => execute_interactive_input_file(options),
+        2 => execute_interactive_output_file(options),
+        3 => execute_interactive_scene_structure(options),
+        4 => execute_interactive_rendering(options),
+        5 => execute_interactive_performance(options),
+        6 => {
+            print!("> Completed interactive mod and Starting to Execute!\n");
+            break;
         }
-    }
-
+        7 => {
+            options.errors.add_serious_error(errors::SeriousErrors::ExplicitExit(
+                String::from("exit typed on interactive mod")));
+            break;
+        },
+        _ => print!("Wrong input\n"),
+    }}
 }
 
 fn execute_interactive_input_file(_options : &mut options::Options) {
 
-    loop {
-
-        print!(
+    loop {  match get_val(
 "\n- Input File Options -
-1) go back
->> ");
-
-        flush_output();
-            
-        match read_int() {
-            1 => break,
-            _ => print!("Wrong input\n"),
-        }
-    }
+0) go back
+", -1) {
+        0 => break,
+        _ => print!("Wrong input\n"),
+    }}
 }
 
-fn execute_interactive_output_file(_options : &mut options::Options) {
-    loop {
-
-        print!(
+fn execute_interactive_output_file(options : &mut options::Options) {
+    
+    loop { match get_val(
 "\n- Output File Options -
-1) go back
->> ");
-
-        flush_output();
-            
-        match read_int() {
-            1 => break,
-            _ => print!("Wrong input\n"),
-        }
-    }
+0) go back
+1) view output file options
+2) file for log
+", -1) {
+        0 => break,
+        1 => print!("- View Output File Options -\n{}", options.options_output.to_string()),
+        2 => options.options_output.file_for_log = get_strnig("Input filename to store image "),
+        _ => print!("Wrong input\n"),
+    }}
 }
 
 fn execute_interactive_scene_structure(_options : &mut options::Options) {
-    loop {
 
-        print!(
+    loop { match get_val(
 "\n- Scene Structure Options -
-1) go back
->> ");
-
-        flush_output();
-            
-        match read_int() {
-            1 => break,
-            _ => print!("Wrong input\n"),
-        }
-    }
+0) go back
+", -1) {
+        0 => break,
+        _ => print!("Wrong input\n"),
+    }}
 }
 
 fn execute_interactive_rendering(_options : &mut options::Options) {
-    loop {
-
-        print!(
+    
+    loop { match get_val(
 "\n- Rendering Options -
-1) go back
->> ");
-
-        flush_output();
-            
-        match read_int() {
-            1 => break,
-            _ => print!("Wrong input\n"),
-        }
-    }
+0) go back
+", -1) {
+        0 => break,
+        _ => print!("Wrong input\n"),
+    }}
 }
 
 fn execute_interactive_performance(_options : &mut options::Options) {
-    loop {
 
-        print!(
+    loop { match get_val(
 "\n- Performance Options -
-1) go back
->> ");
-
-        flush_output();
-            
-        match read_int() {
-            1 => break,
-            _ => print!("Wrong input\n"),
-        }
-    }
+0) go back
+", -1) {
+        0 => break,
+        _ => print!("Wrong input\n"),
+    }}
 }
 
--- src/layer_option/options_output.rs
+++ src/layer_option/options_output.rs
@@ -9,4 +9,10 @@ impl OptionsOutput {
             file_for_log: String::from("output_log.txt"),
         }
     }
+
+    pub fn to_string(&self) -> String {
+        let mut buf = String::new();
+        buf.push_str(&format!("file for log: {}\n", self.file_for_log));
+        return buf;
+    }
 }
